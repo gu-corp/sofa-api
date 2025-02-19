1845a97384811ae329779cefef04ed67e5e4a634
Nguyen Van Thanh <thanhnv@gu-tech.com>
2025-02-19 09:16:20 +0700

    add default config

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,19 @@ import { createSofa } from './sofa.js';
 export { OpenAPI } from './open-api/index.js';
 
 export function useSofa(config: SofaConfig) {
-  return createSofaRouter(createSofa(config));
+  const defaultConfig: Partial<SofaConfig> = {
+    customScalars: {
+      Date: { type: 'string', format: 'date-time' },
+    }
+  }
+  return createSofaRouter(createSofa({
+    ...defaultConfig,
+    ...config,
+    customScalars: {
+      ...defaultConfig.customScalars,
+      ...config.customScalars
+    }
+  }));
 }
 
 export { createSofaRouter, createSofa };
